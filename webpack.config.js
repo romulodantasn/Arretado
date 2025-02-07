309cea5775cada84b1d4989eb59b5b04a65157bb
rmvllo <romulodtsn@gmail.com>
2025-02-07 10:42:20 -0300

    Corrigindo caminhos para GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.ts', 
+  entry: './src/main.ts', // Arquivo principal do seu projeto
   output: {
-    filename: './docs/bundle.js',
+    filename: 'bundle.js', // Arquivo de saída
     path: path.resolve(__dirname, 'docs'), // Pasta onde o arquivo será salvo
-    publicPath: '/arretadov1.0.02/', // Ajuste para o caminho correto no GitHub Pages
+    publicPath: '/Arretado/', // Caminho correto no GitHub Pages
   },
   resolve: {
     extensions: ['.ts', '.js'], // Extensões que o Webpack irá resolver
@@ -19,11 +19,11 @@ module.exports = {
         test: /\.(png|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/sprites/[name][ext][query]'
-        }
+          filename: 'assets/sprites/[name][ext][query]',
+        },
       },
       {
-        test: /\.ts$/,  
+        test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -34,11 +34,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html', // Caminho para o seu arquivo HTML de origem
       filename: 'index.html', // Nome do arquivo de saída na pasta docs
-      publicPath: '/arretadov1.0.02'
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/assets', to: 'assets' }, // Copia os assets da pasta src para a pasta docs/assets
+        { from: 'src/assets', to: 'assets' }, // Copia os assets para a pasta docs/assets
       ],
     }),
   ],
