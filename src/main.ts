9482f49106089f08c82d146ede1b26ddfacc8ecb
rmvllo <romulodtsn@gmail.com>
2025-03-13 17:27:24 -0300

    feat(main.ts): adding nextPhaseScene

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { preloadAssets } from './scenes/preloadAssets';
 import { gameScene } from './scenes/gameScene';
 import { pauseScene } from './scenes/pauseScene';
 import { gameHud } from './objects/ui/gameHud';
+import { nextPhaseScene } from './scenes/nextPhaseScene';
 
-configObject.scene = [bootScene, preloadAssets, gameScene, gameHud, pauseScene];
+configObject.scene = [bootScene, preloadAssets, gameScene, gameHud, pauseScene, nextPhaseScene];
 
 // Inicializando o jogo
 new Phaser.Game(configObject);
