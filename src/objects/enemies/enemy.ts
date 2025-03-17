41540cfb30e2ddfdd5315af3fe69f8e3913fdb20
rmvllo <romulodtsn@gmail.com>
2025-03-17 16:36:27 -0300

    fix(enemy): fixing collision bug of enemy to player
    calling super(scene.physics.world, scene) ensures that the group (presumably a
    Phaser.GameObjects.Group or some similar object) has access to the physics system correctly, i.e. it
    is being bound to the scene's physics world.

--- src/objects/enemies/enemy.ts
+++ src/objects/enemies/enemy.ts
@@ -2,24 +2,18 @@ import Phaser from 'phaser';
 import { gameOptions } from '../../config/gameOptions';
 import { player } from '../player/player';
 
-export class enemyGroup extends Phaser.Physics.Arcade.Sprite {
-  private enemyGroup: Phaser.Physics.Arcade.Group;
-  private enemySprite: Phaser.Physics.Arcade.Sprite;
+export class enemyGroup extends Phaser.Physics.Arcade.Group {
   private player: player;
 
   constructor(scene: Phaser.Scene, player: player) {
-    super(scene, 0, 0, 'enemy');
+    super(scene.physics.world, scene);
     this.player = player;
-
     scene.add.existing(this);
-    scene.physics.add.existing(this);
 
     this.initializeEnemyGroup(scene);
   }
 
   private initializeEnemyGroup(scene: Phaser.Scene) {
-    this.enemyGroup = scene.physics.add.group();
-
     const outerRectangle = new Phaser.Geom.Rectangle(
       -100,
       -100,
@@ -39,22 +33,17 @@ export class enemyGroup extends Phaser.Physics.Arcade.Sprite {
       loop: true,
       callback: () => {
         const spawnPoint = Phaser.Geom.Rectangle.RandomOutside(outerRectangle, innerRectangle);
-        this.enemySprite = scene.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'enemy');
-        this.enemySprite.play('enemy', true);
-        this.enemyGroup.add(this.enemySprite);
+        const enemy = this.create(spawnPoint.x, spawnPoint.y, 'enemy') as Phaser.Physics.Arcade.Sprite;
+        enemy.play('enemy', true);
       },
     });
   }
 
   public updateEnemyMovement(scene: Phaser.Scene) {
-    this.enemyGroup.getChildren().forEach((enemy: Phaser.GameObjects.GameObject) => {
+    this.getChildren().forEach((enemy: Phaser.GameObjects.GameObject) => {
       if (enemy instanceof Phaser.Physics.Arcade.Sprite) {
         scene.physics.moveToObject(enemy, this.player, gameOptions.enemySpeed);
       }
     });
-  }
-
-  public getEnemyGroup() {
-    return this.enemyGroup;
   }
 }
