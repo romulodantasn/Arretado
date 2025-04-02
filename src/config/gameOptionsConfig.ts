b84f1612d0e8a0e59dca81ac1d65a6cc9fee04b0
rmvllo <romulodtsn@gmail.com>
2025-04-02 17:26:10 -0300

    feat(gameoptionsconfig): adding new atributes
    adding player Health and Damage atributes, enemy Damage and time for invulnerabilityDuration for
    colliderComponent

--- src/config/gameOptionsConfig.ts
+++ src/config/gameOptionsConfig.ts
@@ -6,8 +6,12 @@ export const gameOptions = {
   gameBackground: 'gameBackgroundLimbo',
   type: Phaser.AUTO,
   playerSpeed: 600, // velocidade do jogador em pixels por segundo
+  playerHealth: 10, // vida do player
+  playerDamage: 10, // dano do player
   enemySpeed: 50, // velocidade do inimigo, em pixels por segundo
-  bulletSpeed: 100, // velocidade da bala, em pixels por segundo
-  bulletRate: 4500, // rate da bala, balas por milisegundos
   enemyRate: 800, // rate do inimigo, inimigos por milisegundos
+  enemyDamage: 2, // dano do inimigo
+  bulletSpeed: 600, // velocidade da bala, em pixels por segundo
+  bulletRate: 4500, // rate da bala, balas por milisegundos
+  invulnerabilityDuration: 500,
 };
