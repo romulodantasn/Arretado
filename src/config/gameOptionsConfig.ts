e27bb5c90d582132edd39734144bc9c8f7b0e1d8
rmvllo <romulodtsn@gmail.com>
2025-04-07 17:50:49 -0300

    feat(gameoptionsconfig): adding playerMoveSpeed, Lucky, FireRate, CoinGame and removing bulletRate

--- src/config/gameOptionsConfig.ts
+++ src/config/gameOptionsConfig.ts
@@ -5,13 +5,19 @@ export const gameOptions = {
   },
   gameBackground: 'gameBackgroundLimbo',
   type: Phaser.AUTO,
-  playerSpeed: 600, // velocidade do jogador em pixels por segundo
+  playerMoveSpeed: 600, // velocidade do jogador em pixels por segundo
   playerHealth: 10, // vida do player
   playerDamage: 10, // dano do player
+  playerLucky: 0, // chance de crítico do player
+  playerFireRate: 1000, // rate do player, tiros por milisegundos
+  playerCoinGame: 600, // moedas do player
+
   enemySpeed: 50, // velocidade do inimigo, em pixels por segundo
   enemyRate: 800, // rate do inimigo, inimigos por milisegundos
   enemyDamage: 2, // dano do inimigo
+
   bulletSpeed: 600, // velocidade da bala, em pixels por segundo
-  bulletRate: 4500, // rate da bala, balas por milisegundos
+  //bulletRate: 10000, // rate da bala, balas por milisegundos
+
   invulnerabilityDuration: 500,
 };
