dd638ea316d0529462cf2b663d71aaff1bc068aa
rmvllo <romulodtsn@gmail.com>
2025-04-09 18:00:15 -0300

    refactor(gamescene): refactoring scene.launch.gameHud

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,96 +1,98 @@
 import Phaser from 'phaser';
-  import { gameOptions } from '../config/gameOptionsConfig';
-  import { inputManager } from '../components/input/inputManagerComponent';
-  import { player } from '../objects/player/playerObject';
-  import { enemyGroup } from '../objects/enemies/enemyObject';
-  import { collider } from '../components/collider/colliderComponent';
-  import { bulletComponent } from '../objects/bullet/bulletComponent';
-  import { healthComponent } from '../components/health/healthComponent';
-  import { globalEventEmitter } from '../components/events/globalEventEmitter';
-  
-  export class gameScene extends Phaser.Scene {
-    #keys: any;
-    #player: player;
-    #enemy: enemyGroup;
-    #bullet: bulletComponent;
-    #collisionHandler: collider;
-    #health: healthComponent;
-    #reticle: Phaser.GameObjects.Sprite;
-  
-    constructor() {
-      super({ key: 'gameScene' });
-    }
-  
-    preload() {
-      this.load.pack('asset_pack', 'assets/data/assets.json');
-    }
-  
-    create() {
-      console.log('gameScene carregado');
+import { gameOptions } from '../config/gameOptionsConfig';
+import { inputManager } from '../components/input/inputManagerComponent';
+import { player } from '../objects/player/playerObject';
+import { enemyGroup } from '../objects/enemies/enemyObject';
+import { collider } from '../components/collider/colliderComponent';
+import { bulletComponent } from '../objects/bullet/bulletComponent';
+import { healthComponent } from '../components/health/healthComponent';
+import { globalEventEmitter } from '../components/events/globalEventEmitter';
 
-      this.#health = new healthComponent(); 
-  
-      this.scene.launch('gameHud', { health: this.#health }); 
-  
-      this.add
-        .image(0, 0, 'gameBackgroundLimbo')
-        .setOrigin(0, 0)
-        .setDisplaySize(gameOptions.gameSize.width, gameOptions.gameSize.height);
-  
-      inputManager.setupControls(this);
-      this.#keys = inputManager.getKeys();
-  
-      this.#player = new player(this, gameOptions.gameSize.width / 2, gameOptions.gameSize.height / 2);
-  
-      this.#enemy = new enemyGroup(this, this.#player);
-  
-      this.#bullet = new bulletComponent(this, this.#player, this.#enemy, this.#reticle, this.input.activePointer);
-      this.#bullet.reticleMovement();
-      this.#bullet.setupShooting();
-  
-      this.#collisionHandler = new collider(this, this.#player, this.#enemy, this.#health);
-      this.#collisionHandler.create();
-  
-      this.time.delayedCall(100, () => {
-        this.scene.launch('healthUi', { emitter: globalEventEmitter, health: this.#health }); 
-      });
-      console.log('healthUi carregada');
-  
-      this.events.on('nextPhase', this.triggerNextPhase, this);
-    }
-  
-    update() {
-      this.#bullet.containReticle();
-      this.handlePause();
-      this.#player.update();
-      this.#enemy.updateEnemyMovement(this);
-    }
-  
-    private handlePause() {
-      if (!Phaser.Input.Keyboard.JustDown(this.#keys.pause)) return;
-      const gameScene = 'gameScene';
-      const gameHud = 'gameHud';
-      const pauseScene = 'pauseScene';
-  
-      const isGamePaused = this.scene.isPaused(gameScene);
-      const isHudPaused = this.scene.isPaused(gameHud);
-      const isHudActive = this.scene.isActive(gameHud);
-  
-      if (isGamePaused) {
-        console.log('Jogo retomado');
-        this.scene.resume(gameScene);
-        if (isHudPaused) this.scene.resume(gameHud);
-        this.scene.stop(pauseScene);
-      } else {
-        console.log('Jogo Pausado');
-        this.scene.pause(gameScene);
-        if (isHudActive) this.scene.pause(gameHud);
-        this.scene.launch(pauseScene);
+export class gameScene extends Phaser.Scene {
+  #keys: any;
+  #player: player;
+  #enemy: enemyGroup;
+  #bullet: bulletComponent;
+  #collisionHandler: collider;
+  #health: healthComponent;
+  #reticle: Phaser.GameObjects.Sprite;
+
+  constructor() {
+    super({ key: 'gameScene' });
+  }
+
+  preload() {
+    this.load.pack('asset_pack', 'assets/data/assets.json');
+  }
+
+  create() {
+    console.log('gameScene carregado');
+
+    this.#health = new healthComponent();
+
+    this.scene.launch('gameHud');
+
+    this.add
+      .image(0, 0, 'gameBackgroundLimbo')
+      .setOrigin(0, 0)
+      .setDisplaySize(gameOptions.gameSize.width, gameOptions.gameSize.height);
+
+    inputManager.setupControls(this);
+    this.#keys = inputManager.getKeys();
+
+    this.#player = new player(this, gameOptions.gameSize.width / 2, gameOptions.gameSize.height / 2);
+
+    this.#enemy = new enemyGroup(this, this.#player);
+
+    this.#bullet = new bulletComponent(this, this.#player, this.#enemy, this.#reticle, this.input.activePointer);
+    this.#bullet.reticleMovement();
+    this.#bullet.setupShooting();
+
+    this.#collisionHandler = new collider(this, this.#player, this.#enemy, this.#health);
+    this.#collisionHandler.create();
+
+    this.time.delayedCall(100, () => {
+      if (!this.scene.isActive('healthUi')) {
+        this.scene.launch('healthUi', { emitter: globalEventEmitter, health: this.#health });
       }
-    }
-  
-    private triggerNextPhase() {
-      console.log('Avançando para a próxima fase');
-      this.scene.start('nextPhaseScene');
+    });
+    console.log('healthUi carregada');
+
+    this.events.on('nextPhase', this.triggerNextPhase, this);
+  }
+
+  update() {
+    this.#bullet.containReticle();
+    this.handlePause();
+    this.#player.update();
+    this.#enemy.updateEnemyMovement(this);
+  }
+
+  private handlePause() {
+    if (!Phaser.Input.Keyboard.JustDown(this.#keys.pause)) return;
+    const gameScene = 'gameScene';
+    const gameHud = 'gameHud';
+    const pauseScene = 'pauseScene';
+
+    const isGamePaused = this.scene.isPaused(gameScene);
+    const isHudPaused = this.scene.isPaused(gameHud);
+    const isHudActive = this.scene.isActive(gameHud);
+
+    if (isGamePaused) {
+      console.log('Jogo retomado');
+      this.scene.resume(gameScene);
+      if (isHudPaused) this.scene.resume(gameHud);
+      this.scene.stop(pauseScene);
+    } else {
+      console.log('Jogo Pausado');
+      this.scene.pause(gameScene);
+      if (isHudActive) this.scene.pause(gameHud);
+      this.scene.launch(pauseScene);
     }
   }
+
+  private triggerNextPhase() {
+    console.log('Avançando para a próxima fase');
+    this.scene.start('nextPhaseScene');
+  }
+}
