3c67fda101c290f8ab1870e422c9cb3ddf67087e
rmvllo <romulodtsn@gmail.com>
2025-04-09 17:52:31 -0300

    feat(gameoptions): creating playerHelthPoints for playerStats and adding currentWave and currentAct

--- src/config/gameOptionsConfig.ts
+++ src/config/gameOptionsConfig.ts
@@ -10,14 +10,15 @@ export const gameOptions = {
   playerDamage: 10, // dano do player
   playerLucky: 0, // chance de crítico do player
   playerFireRate: 1000, // rate do player, tiros por milisegundos
-  playerCoinGame: 600, // moedas do player
-
+  playerCoinGame: 0, // moedas do player
+  playerHealthPoints: 10,
   enemySpeed: 50, // velocidade do inimigo, em pixels por segundo
   enemyRate: 800, // rate do inimigo, inimigos por milisegundos
   enemyDamage: 2, // dano do inimigo
 
   bulletSpeed: 600, // velocidade da bala, em pixels por segundo
-  //bulletRate: 10000, // rate da bala, balas por milisegundos
 
   invulnerabilityDuration: 500,
+  currentWave: 1,
+  currentAct: 1,
 };
