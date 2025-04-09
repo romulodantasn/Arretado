7226c2b391a9edca66f61bd51f3f400e8aea8e63
rmvllo <romulodtsn@gmail.com>
2025-04-09 17:47:31 -0300

    refactor(itemscene): refactoring and making itemScene cleaner

---
+++ src/scenes/itemScene.ts
@@ -0,0 +1,20 @@
+import Phaser from 'phaser';
+import { itemsDisplayUi } from '../objects/ui/itemsDisplayUi';
+
+export class itemScene extends Phaser.Scene {
+  #itemsDisplayUi: itemsDisplayUi;
+
+  constructor() {
+    super({
+      key: 'itemScene',
+    });
+  }
+
+  create() {
+    console.log('upgradeScene Carregada');
+
+    this.#itemsDisplayUi = new itemsDisplayUi(this, 0, 0);
+    this.add.existing(this.#itemsDisplayUi);
+    this.#itemsDisplayUi.create();
+  }
+}
