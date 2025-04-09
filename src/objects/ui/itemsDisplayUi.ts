81134dbf7ac76943f25ebdff50d2e468b483d17a
rmvllo <romulodtsn@gmail.com>
2025-04-09 17:59:02 -0300

    feat(itemsdisplayui): implementing all the containers, itens and logic for itemDisplay
    creating container and structuring items, sentences, description, cost, etc. addind logic for buying
    item (checking if theres enough playerCoinGame for buy the item or not. creating container that
    stores all the player stats     after bought a item

--- src/objects/ui/itemsDisplayUi.ts
+++ src/objects/ui/itemsDisplayUi.ts
@@ -1,129 +1,153 @@
 import Phaser from 'phaser';
 import { gameOptions } from '../../config/gameOptionsConfig';
-import {
-  damageItems,
-  lifeItems,
-  moveSpeedItems,
-  luckyItems,
-  itemsContainer,
-} from '../../objects/upgrades/itemsContainer';
+import { gameHud } from './gameHudUi';
+import { damageItems, lifeItems, moveSpeedItems, luckyItems, itemsContainer } from '../upgrades/itemsContainer';
 
 export class itemsDisplayUi extends Phaser.GameObjects.Container {
   #damageItems: itemsContainer[] = damageItems;
   #lifeItems: itemsContainer[] = lifeItems;
   #moveSpeedItems: itemsContainer[] = moveSpeedItems;
   #luckyItems: itemsContainer[] = luckyItems;
-  #upgradeContainers: Phaser.GameObjects.Container[];
   #upgradeOptions: itemsContainer[] = [
     ...this.#damageItems,
     ...this.#lifeItems,
     ...this.#moveSpeedItems,
     ...this.#luckyItems,
   ];
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    super(scene, x, y);
+  }
 
   create() {
     console.log('itemsDisplay Carregada');
-    this.displayUpgradeOptions();
+
+    const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
+    const titleText = ['Eai, Caba. O que vai ser?'];
+    const statsTitleText = ['Atributos'];
+    this.scene.add.text(960, 200, titleText, textStyle).setFontSize(48).setAlign('center').setOrigin(0.5);
+    this.scene.add.text(1800, 300, statsTitleText, textStyle).setFontSize(40).setAlign('center').setOrigin(0.5);
+    this.scene.scene.launch('gameHud', { elementsToShow: ['coins', 'wave'] });
+
+    this.playerStatsContainer(1800, 600);
+    this.containerItems();
   }
 
   // função pra achar a imagem pelo imageKey
-  private findUpgradeOptionByImageKey(items: itemsContainer[], imageKey: string): itemsContainer | undefined {
+  public findUpgradeOptionByImageKey(items: itemsContainer[], imageKey: string): itemsContainer | undefined {
     return items.find((item) => item.imageKey === imageKey);
   }
 
-  private displayUpgradeOptions() {
-    // Create the background containers
-    const upgradeBgDamage = this.createUpgradeContainer(200, 600, 0x333333, this.#damageItems, 'damage');
-    const upgradeBgLife = this.createUpgradeContainer(600, 600, 0x333333, this.#lifeItems, 'life');
-    const upgradeBgMoveSpeed = this.createUpgradeContainer(1000, 600, 0x333333, this.#moveSpeedItems, 'moveSpeed');
-    const upgradeBgLucky = this.createUpgradeContainer(1400, 600, 0x333333, this.#luckyItems, 'lucky');
+  private containerItems() {
+    this.createItemContainer(200, 600, 0x333333, this.#damageItems, 'damage');
+    this.createItemContainer(600, 600, 0x333333, this.#lifeItems, 'life');
+    this.createItemContainer(1000, 600, 0x333333, this.#moveSpeedItems, 'moveSpeed');
+    this.createItemContainer(1400, 600, 0x333333, this.#luckyItems, 'lucky');
   }
 
-  private createUpgradeContainer(
+  public createItemContainer(
     x: number,
     y: number,
     color: number,
     items: itemsContainer[],
     type: string
   ): Phaser.GameObjects.Container {
-    const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
-    const titleText = ['Eai, Caba. O que vai ser?'];
-    this.scene.add.text(960, 200, titleText, textStyle).setFontSize(48).setAlign('center').setOrigin(0.5);
-    const upgradeBg = this.scene.add.rectangle(0, 0, 350, 580, color).setStrokeStyle(2, 0xffffff);
-    const container = this.scene.add.container(x, y, [upgradeBg]);
-    this.#upgradeContainers.push(container);
+    const itemBg = this.scene.add.rectangle(0, 0, 350, 580, color).setStrokeStyle(2, 0xffffff);
+    const container = this.scene.add.container(x, y, [itemBg]);
     container.setSize(350, 580);
     container.setInteractive(new Phaser.Geom.Rectangle(-175, -290, 350, 580), Phaser.Geom.Rectangle.Contains);
 
     container.on('pointerover', () => {
-      upgradeBg.setFillStyle(0x555555);
+      itemBg.setFillStyle(0x555555);
     });
     container.on('pointerout', () => {
-      upgradeBg.setFillStyle(color);
+      itemBg.setFillStyle(color);
     });
 
-    items.forEach((item, index) => {
-      const image = this.scene.add.image(0, -100, item.imageKey).setScale(0.5);
+    if (items.length > 0) {
+      const item = items[0];
+      const image = this.scene.add.image(0, -200, item.imageKey).setScale(0.5).setDisplaySize(134, 134);
+      const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
       const nameText = this.scene.add
-        .text(0, 0, item.name, { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 })
+        .text(0, -80, item.name, { ...textStyle, align: 'center', wordWrap: { width: 300 } })
+        .setOrigin(0.5)
+        .setFontSize(36);
+      const typeText = this.scene.add
+        .text(0, -30, item.type, { ...textStyle, align: 'center', wordWrap: { width: 200 } })
         .setOrigin(0.5)
         .setFontSize(32);
       const descriptionText = this.scene.add
-        .text(0, 50, item.description, {
-          fontFamily: 'Cordelina',
-          color: '#ffffff',
-          stroke: '#000000',
-          strokeThickness: 4,
-        })
+        .text(0, 40, item.description, { ...textStyle, align: 'center', wordWrap: { width: 330 } })
+        .setOrigin(0.5)
+        .setFontSize(26);
+      const sentenceText = this.scene.add
+        .text(0, 150, item.sentence, { ...textStyle, align: 'center', wordWrap: { width: 330 } })
         .setOrigin(0.5)
         .setFontSize(24);
       const costText = this.scene.add
-        .text(0, 100, `Cost: ${item.cost}`, {
-          fontFamily: 'Cordelina',
-          color: '#ffffff',
-          stroke: '#000000',
-          strokeThickness: 4,
-        })
+        .text(0, 220, `Cost: ${item.cost}`, { ...textStyle, align: 'center' })
         .setOrigin(0.5)
-        .setFontSize(24);
-      container.add([image, nameText, descriptionText, costText]);
+        .setFontSize(32);
+
+      container.add([image, nameText, typeText, descriptionText, sentenceText, costText]);
 
       container.on('pointerdown', () => {
+        const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
+
         if (gameOptions.playerCoinGame >= item.cost) {
           item.effect();
           gameOptions.playerCoinGame -= item.cost;
-          console.log(`Upgrade ${item.name} applied!`);
-          this.scene.scene.start('gameScene');
+          const buyText = this.scene.add
+            .text(x, y + 320, `Item ${item.name} comprado!`, { ...textStyle, color: '#ff0000' })
+            .setOrigin(0.5)
+            .setFontSize(28);
+          console.log(`Item ${item.name} comprado!
+            Iniciando a proxima onda...`);
+          this.scene.time.delayedCall(3500, () => this.scene.scene.start('gameScene'));
         } else {
-          console.log('Not enough resources!');
+          console.log('Ta liso, Caba! Num tem moeda suficiente.');
+          const noMoneyText = this.scene.add
+            .text(x, y + 320, 'Ta liso, caba! Num tem moeda suficiente.', { ...textStyle, color: '#ff0000' })
+            .setOrigin(0.5)
+            .setFontSize(28);
+          this.scene.time.delayedCall(3500, () => noMoneyText.destroy());
         }
       });
-    });
+    } else {
+      console.warn('Nenhum item encontrado para o tipo:', type);
+    }
+
     return container;
   }
 
-  /*private setupInput() {
-    const keyboard = this.input.keyboard as Phaser.Input.Keyboard.KeyboardPlugin;
-    const upKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-    const downKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-    const enterKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+  private playerStatsContainer(x: number, y: number) {
+    const health = gameOptions.playerHealthPoints;
+    const damage = gameOptions.playerDamage;
+    const moveSpeed = gameOptions.playerMoveSpeed;
+    const lucky = gameOptions.playerLucky;
 
-    upKey.on('down', () => {
-      this.#selectedUpgradeIndex = Math.max(0, this.#selectedUpgradeIndex - 1);
-      this.highlightSelectedUpgrade();
-    });
+    const statsBg = this.scene.add.rectangle(0, 0, 200, 400).setStrokeStyle(2, 0xffffff);
+    const container = this.scene.add.container(x, y, [statsBg]);
+    container.setSize(200, 400);
+    container.setInteractive(new Phaser.Geom.Rectangle(-175, -290, 200, 400), Phaser.Geom.Rectangle.Contains);
 
-    downKey.on('down', () => {
-      this.#selectedUpgradeIndex = Math.min(this.#upgradeOptions.length - 1, this.#selectedUpgradeIndex + 1);
-      this.highlightSelectedUpgrade();
+    const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
+    const healthText = this.scene.add.text(0, -150, `Vida: ${health}`, textStyle).setOrigin(0.5).setFontSize(36);
+    const damageText = this.scene.add.text(0, -75, `Dano: ${damage}`, textStyle).setOrigin(0.5).setFontSize(36);
+    const moveSpeedText = this.scene.add
+      .text(0, 0, `Velocidade: ${moveSpeed}`, textStyle)
+      .setOrigin(0.5)
+      .setFontSize(28);
+    const luckyText = this.scene.add.text(0, 75, `Sorte: ${lucky}`, textStyle).setOrigin(0.5).setFontSize(36);
+    container.on('pointerover', () => {
+      statsBg.setFillStyle(0x555555);
     });
-
-    enterKey.on('down', () => {
-      this.applyUpgrade();
+    container.on('pointerout', () => {
+      statsBg.setFillStyle(0x333333);
     });
+    container.add([healthText, damageText, moveSpeedText, luckyText]);
   }
 
-  private highlightSelectedUpgrade() {
+  /*private highlightSelectedUpgrade() {
     this.#upgradeTexts.forEach((text, index) => {
       if (index === this.#selectedUpgradeIndex) {
         text.setColor('#ffff00'); // Highlight color
