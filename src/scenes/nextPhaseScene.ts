a15f49ac60098b46ef2ac5e03c6c7db9458744dc
rmvllo <romulodtsn@gmail.com>
2025-04-10 18:10:17 -0300

    test(nextphasescene): testing flow of nextPhaseScene  and removing increase wave aspects

--- src/scenes/nextPhaseScene.ts
+++ src/scenes/nextPhaseScene.ts
@@ -1,10 +1,7 @@
 import Phaser from 'phaser';
-import { gameOptions } from '../config/gameOptionsConfig';
 import { gameHud } from '../objects/ui/gameHudUi';
 
 export class nextPhaseScene extends Phaser.Scene {
-  private nextPhaseText: Phaser.GameObjects.Text;
-
   constructor() {
     super({
       key: 'nextPhaseScene',
@@ -20,6 +17,7 @@ export class nextPhaseScene extends Phaser.Scene {
     const hudScene = this.scene.get('gameHud') as gameHud;
     const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
     const nextPhaseText = ['Eita caba danado! Sobreviveu! Avançando para a próxima fase...'];
+    this.add.text(980, 510, nextPhaseText, textStyle).setFontSize(36).setAlign('center').setOrigin(0.5);
 
     if (hudScene) {
       hudScene.advanceWaveCount();
@@ -27,17 +25,11 @@ export class nextPhaseScene extends Phaser.Scene {
     this.time.delayedCall(2000, () => {
       this.scene.stop('healthUi');
       this.scene.stop('gameHud');
-      this.scene.stop('gameScene');
-      this.scene.stop('nextPhaseScene');
+      // this.scene.stop('nextPhaseScene');
       this.scene.stop('gameScene');
       this.scene.start('itemScene');
     });
 
-    this.add.text(980, 510, nextPhaseText, textStyle).setFontSize(36).setAlign('center').setOrigin(0.5);
-    gameOptions.enemyRate -= 100;
-    gameOptions.enemySpeed += 10;
-    console.log('Eita caba danado! Sobreviveu! Avançando para a próxima fase..');
-    console.log('enemySpeedUpdated: ' + gameOptions.enemySpeed);
-    console.log('enemyRateUpdated: ' + gameOptions.enemyRate);
+    console.log('Avançando para a próxima fase..');
   }
 }
