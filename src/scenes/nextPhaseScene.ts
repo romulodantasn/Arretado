57e053ba857ce268485e4bd4356d9b2ad546e1ca
rmvllo <romulodtsn@gmail.com>
2025-04-15 14:00:35 -0300

    fix(nextphasescene): fixing bug of this.data null when nextPhase is on
    adding logic to stops the scenes when nextPhase is started, preventing bugs and blocking any action
    from the user

--- src/scenes/nextPhaseScene.ts
+++ src/scenes/nextPhaseScene.ts
@@ -1,6 +1,5 @@
 import Phaser from 'phaser';
 import { gameHud } from '../objects/ui/gameHudUi';
-
 export class nextPhaseScene extends Phaser.Scene {
   constructor() {
     super({
@@ -10,6 +9,22 @@ export class nextPhaseScene extends Phaser.Scene {
 
   create() {
     this.nextPhase();
+ 
+    // if (this.scene.isActive('gameScene')) {
+      this.scene.stop('gameScene');
+    // }
+    if (this.scene.isActive('gameHud')) {
+      this.scene.stop('gameHud');
+    }
+    if (this.scene.isActive('healthUi')) {
+       this.scene.stop('healthUi');
+    }
+    if (this.scene.isActive('PauseScene')) {
+      this.scene.stop('PauseScene');
+    }
+    this.input.enabled = false;
+    this.input.setDefaultCursor('none');
+    
     console.log('nextPhaseScene Carregada');
   }
 
@@ -17,16 +32,13 @@ export class nextPhaseScene extends Phaser.Scene {
     const hudScene = this.scene.get('gameHud') as gameHud;
     const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
     const nextPhaseText = ['Eita caba danado! Sobreviveu! Avançando para a próxima fase...'];
-    this.add.text(980, 510, nextPhaseText, textStyle).setFontSize(36).setAlign('center').setOrigin(0.5);
+    this.add.text( this.scale.width / 2, this.scale.height / 2 + 3, nextPhaseText, textStyle).setFontSize(36).setAlign('center').setOrigin(0.5);
 
     if (hudScene) {
       hudScene.advanceWaveCount();
     }
     this.time.delayedCall(2000, () => {
-      this.scene.stop('healthUi');
-      this.scene.stop('gameHud');
-      // this.scene.stop('nextPhaseScene');
-      this.scene.stop('gameScene');
+      this.scene.stop('nextPhaseScene');
       this.scene.start('itemScene');
     });
 
