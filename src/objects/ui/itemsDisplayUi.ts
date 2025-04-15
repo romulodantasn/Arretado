ca4aecb5a623a19057de4357ab71bd7de8276e7b
rmvllo <romulodtsn@gmail.com>
2025-04-15 13:54:38 -0300

    feat(itemsdisplayui): adding a function to shuffle items, making them random after each wave

--- src/objects/ui/itemsDisplayUi.ts
+++ src/objects/ui/itemsDisplayUi.ts
@@ -1,44 +1,98 @@
-import Phaser from 'phaser';
-import { gameOptions } from '../../config/gameOptionsConfig';
-import { damageItems, lifeItems, moveSpeedItems, luckyItems, itemsContainer } from '../upgrades/itemsContainer';
+import Phaser from "phaser";
+import { gameOptions } from "../../config/gameOptionsConfig";
+import {
+  damageItems,
+  lifeItems,
+  moveSpeedItems,
+  luckyItems,
+  itemsContainer,
+} from "../upgrades/itemsContainer";
+
+function shuffleItems(items: itemsContainer[]) {
+  for (let i = items.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [items[i], items[j]] = [items[j], items[i]];
+  }
+  return items;
+}
 
 export class itemsDisplayUi extends Phaser.GameObjects.Container {
-  textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
+  textStyle = {
+    fontFamily: "Cordelina",
+    color: "#ffffff",
+    stroke: "#000000",
+    strokeThickness: 4,
+  };
 
-  #damageItems: itemsContainer[] = damageItems;
-  #lifeItems: itemsContainer[] = lifeItems;
-  #moveSpeedItems: itemsContainer[] = moveSpeedItems;
-  #luckyItems: itemsContainer[] = luckyItems;
+  #damageItems: itemsContainer[] = [...damageItems];
+  #lifeItems: itemsContainer[] = [...lifeItems];
+  #moveSpeedItems: itemsContainer[] = [...moveSpeedItems];
+  #luckyItems: itemsContainer[] = [...luckyItems];
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y);
   }
 
   create() {
-    console.log('itemsDisplay Carregada');
-    this.scene.input.setDefaultCursor('default');
+    console.log("itemsDisplay Carregada");
+    this.scene.input.setDefaultCursor("default");
 
-    const titleText = ['Eai, Caba. O que vai ser?'];
-    const statsTitleText = ['Atributos'];
-    this.scene.add.text(860, 200, titleText, this.textStyle).setFontSize(48).setAlign('center').setOrigin(0.5);
-    this.scene.add.text(1775, 350, statsTitleText, this.textStyle).setFontSize(40).setAlign('center').setOrigin(0.5);
-    this.scene.scene.launch('gameHud', { elementsToShow: ['coins', 'wave'] });
+    const titleText = ["Eai, Caba. O que vai ser?"];
+    const statsTitleText = ["Atributos"];
+    this.scene.add
+      .text(860, 200, titleText, this.textStyle)
+      .setFontSize(48)
+      .setAlign("center")
+      .setOrigin(0.5);
+    this.scene.add
+      .text(1775, 350, statsTitleText, this.textStyle)
+      .setFontSize(40)
+      .setAlign("center")
+      .setOrigin(0.5);
+    this.scene.scene.stop("gameHud");
+    this.scene.scene.launch("gameHud", { elementsToShow: ["coins", "wave"] });
 
     this.playerStatsContainer(1800, 600);
     this.containerItems();
     this.buttonNextPhase();
   }
 
-  // função pra achar a imagem pelo imageKey
-  public findUpgradeOptionByImageKey(items: itemsContainer[], imageKey: string): itemsContainer | undefined {
-    return items.find((item) => item.imageKey === imageKey);
-  }
-
   private containerItems() {
-    this.createItemContainer(200, 600, 0x333333, this.#damageItems, 'damage');
-    this.createItemContainer(600, 600, 0x333333, this.#lifeItems, 'life');
-    this.createItemContainer(1000, 600, 0x333333, this.#moveSpeedItems, 'moveSpeed');
-    this.createItemContainer(1400, 600, 0x333333, this.#luckyItems, 'lucky');
+    shuffleItems(this.#damageItems);
+    shuffleItems(this.#lifeItems);
+    shuffleItems(this.#moveSpeedItems);
+    shuffleItems(this.#luckyItems);
+
+    if (this.#damageItems.length > 0) {
+      this.createItemContainer(200, 600, 0x333333, this.#damageItems, "damage");
+    } else {
+      console.warn("Não há itens de dano para exibir.");
+      
+    }
+
+    if (this.#lifeItems.length > 0) {
+      this.createItemContainer(600, 600, 0x333333, this.#lifeItems, "life");
+    } else {
+      console.warn("Não há itens de vida para exibir.");
+    }
+
+    if (this.#moveSpeedItems.length > 0) {
+      this.createItemContainer(
+        1000,
+        600,
+        0x333333,
+        this.#moveSpeedItems,
+        "moveSpeed"
+      );
+    } else {
+      console.warn("Não há itens de velocidade para exibir.");
+    }
+
+    if (this.#luckyItems.length > 0) {
+      this.createItemContainer(1400, 600, 0x333333, this.#luckyItems, "lucky");
+    } else {
+      console.warn("Não há itens de sorte para exibir.");
+    }
   }
 
   public createItemContainer(
@@ -47,74 +101,114 @@ export class itemsDisplayUi extends Phaser.GameObjects.Container {
     color: number,
     items: itemsContainer[],
     type: string
-  ): Phaser.GameObjects.Container {
+  ): Phaser.GameObjects.Container | undefined {
     const itemBg = this.scene.add
       .rectangle(0, 0, 350, 580, color)
       .setStrokeStyle(2, 0xffffff)
       .setInteractive({ useHandCursor: true });
     const containerItem = this.scene.add.container(x, y, [itemBg]);
 
-    containerItem.setInteractive(new Phaser.Geom.Rectangle(-175, -290, 350, 580), Phaser.Geom.Rectangle.Contains);
+    containerItem.setInteractive(
+      new Phaser.Geom.Rectangle(-175, -290, 350, 580),
+      Phaser.Geom.Rectangle.Contains
+    );
 
-    itemBg.on('pointerover', () => {
+    itemBg.on("pointerover", () => {
       itemBg.setFillStyle(0x555555);
     });
-    itemBg.on('pointerout', () => {
+    itemBg.on("pointerout", () => {
       itemBg.setFillStyle(color);
     });
 
-    if (items.length > 0) {
-      const item = items[0];
-      const image = this.scene.add.image(0, -200, item.imageKey).setScale(0.5).setDisplaySize(134, 134);
-      const textStyle = { fontFamily: 'Cordelina', color: '#ffffff', stroke: '#000000', strokeThickness: 4 };
-      const nameText = this.scene.add
-        .text(0, -80, item.name, { ...textStyle, align: 'center', wordWrap: { width: 300 } })
-        .setOrigin(0.5)
-        .setFontSize(36);
-      const typeText = this.scene.add
-        .text(0, -30, item.type, { ...textStyle, align: 'center', wordWrap: { width: 200 } })
-        .setOrigin(0.5)
-        .setFontSize(32);
-      const descriptionText = this.scene.add
-        .text(0, 40, item.description, { ...textStyle, align: 'center', wordWrap: { width: 330 } })
-        .setOrigin(0.5)
-        .setFontSize(26);
-      const sentenceText = this.scene.add
-        .text(0, 150, item.sentence, { ...textStyle, align: 'center', wordWrap: { width: 330 } })
-        .setOrigin(0.5)
-        .setFontSize(24);
-      const costText = this.scene.add
-        .text(0, 220, `Cost: ${item.cost}`, { ...textStyle, align: 'center' })
-        .setOrigin(0.5)
-        .setFontSize(32);
+    const item = items[0];
+    const image = this.scene.add
+      .image(0, -200, item.imageKey)
+      .setScale(0.5)
+      .setDisplaySize(134, 134);
+    const textStyle = {
+      fontFamily: "Cordelina",
+      color: "#ffffff",
+      stroke: "#000000",
+      strokeThickness: 4,
+    };
+    const nameText = this.scene.add
+      .text(0, -80, item.name, {
+        ...textStyle,
+        align: "center",
+        wordWrap: { width: 300 },
+      })
+      .setOrigin(0.5)
+      .setFontSize(36);
+    const typeText = this.scene.add
+      .text(0, -30, item.type, {
+        ...textStyle,
+        align: "center",
+        wordWrap: { width: 200 },
+      })
+      .setOrigin(0.5)
+      .setFontSize(32);
+    const descriptionText = this.scene.add
+      .text(0, 40, item.description, {
+        ...textStyle,
+        align: "center",
+        wordWrap: { width: 330 },
+      })
+      .setOrigin(0.5)
+      .setFontSize(26);
+    const sentenceText = this.scene.add
+      .text(0, 150, item.sentence, {
+        ...textStyle,
+        align: "center",
+        wordWrap: { width: 330 },
+      })
+      .setOrigin(0.5)
+      .setFontSize(24);
+    const costText = this.scene.add
+      .text(0, 220, `Preco: ${item.cost}`, { ...textStyle, align: "center" })
+      .setOrigin(0.5)
+      .setFontSize(32);
 
-      containerItem.add([image, nameText, typeText, descriptionText, sentenceText, costText]);
+    containerItem.add([
+      image,
+      nameText,
+      typeText,
+      descriptionText,
+      sentenceText,
+      costText,
+    ]);
 
-      itemBg.on('pointerdown', () => {
-        if (gameOptions.playerCoinGame >= item.cost) {
-          item.effect();
-          gameOptions.playerCoinGame -= item.cost;
-          this.scene.game.events.emit('coinsUpdated', gameOptions.playerCoinGame);
-          console.log(`Evento de compra emitido ${gameOptions.playerCoinGame} moedas`);
-          const buyText = this.scene.add
-            .text(x, y + 320, `Item ${item.name} comprado!`, { ...textStyle, color: '#008000' })
-            .setOrigin(0.5)
-            .setFontSize(28);
-          console.log(`Item ${item.name} comprado`);
-
-          this.scene.time.delayedCall(3500, () => buyText.destroy());
-        } else {
-          console.log('Not enough money to buy item');
-          const noMoneyText = this.scene.add
-            .text(x, y + 320, 'Ta liso, caba! Num tem moeda suficiente.', { ...textStyle, color: '#ff0000' })
-            .setOrigin(0.5)
-            .setFontSize(28);
-          this.scene.time.delayedCall(3500, () => noMoneyText.destroy());
-        }
-      });
-    } else {
-      console.warn('Nenhum item encontrado para o tipo:', type);
-    }
+    itemBg.on("pointerdown", () => {
+      if (gameOptions.playerCoinGame >= item.cost) {
+        item.effect();
+        gameOptions.playerCoinGame -= item.cost;
+        this.scene.game.events.emit("buyUpdatedCoin", gameOptions.playerCoinGame);
+        
+        console.log(
+          `Evento de compra emitido ${gameOptions.playerCoinGame} moedas`
+        );
+        const buyText = this.scene.add
+          .text(x, y + 320, `Item ${item.name} comprado!`, {
+            ...textStyle,
+            color: "#008000",
+          })
+          .setOrigin(0.5)
+          .setFontSize(28);
+        console.log(`Item ${item.name} comprado`);
+        itemBg.disableInteractive();
+        itemBg.setFillStyle(0x1a1a1a);
+        this.scene.time.delayedCall(2500, () => buyText.destroy());
+      } else {
+        console.log("Moeda insuficiente");
+        const noMoneyText = this.scene.add
+          .text(x, y + 320, "Ta liso, caba! Num tem moeda suficiente.", {
+            ...textStyle,
+            color: "#ff0000",
+          })
+          .setOrigin(0.5)
+          .setFontSize(28);
+        this.scene.time.delayedCall(2500, () => noMoneyText.destroy());
+      }
+    });
     return containerItem;
   }
 
@@ -124,51 +218,68 @@ export class itemsDisplayUi extends Phaser.GameObjects.Container {
       .setStrokeStyle(2, 0xffffff)
       .setInteractive({ useHandCursor: true });
     const buttonText = this.scene.add
-      .text(1700, 975, 'Próxima Onda', this.textStyle)
+      .text(1700, 975, "Próxima Onda", this.textStyle)
       .setFontSize(48)
-      .setAlign('center')
+      .setAlign("center")
       .setOrigin(0.5);
 
-    button.on('pointerover', () => {
+    button.on("pointerover", () => {
       button.setFillStyle(0x555555);
     });
-    button.on('pointerout', () => {
+    button.on("pointerout", () => {
       button.setFillStyle(0x333333);
     });
 
-    button.on('pointerdown', () => {
-      buttonText.setColor('green');
+    button.on("pointerdown", () => {
+      buttonText.setColor("green");
       this.scene.time.delayedCall(1000, () => {
-        buttonText.setColor('#ffffff');
+        buttonText.setColor("#ffffff");
       });
-      this.scene.scene.start('gameScene');
-      this.scene.scene.start('gameHud', { elementsToShow: ['coins', 'wave', 'act', 'timer', 'gun'] });
+      this.scene.scene.start("gameScene");
+      this.scene.scene.start("gameHud", {
+        elementsToShow: ["coins", "wave", "act", "timer", "gun"],
+      });
       gameOptions.currentWave++;
       gameOptions.enemyRate -= 100;
       gameOptions.enemySpeed += 10;
-      console.log('enemySpeedUpdated: ' + gameOptions.enemySpeed);
-      console.log('enemyRateUpdated: ' + gameOptions.enemyRate);
+      console.log("enemySpeedUpdated: " + gameOptions.enemySpeed);
+      console.log("enemyRateUpdated: " + gameOptions.enemyRate);
     });
   }
 
   private playerStatsContainer(x: number, y: number) {
-    const health = gameOptions.playerHealthPoints;
+    const health = gameOptions.playerHealth;
     const damage = gameOptions.playerDamage;
     const moveSpeed = gameOptions.playerMoveSpeed;
     const lucky = gameOptions.playerLucky;
 
-    const statsBg = this.scene.add.rectangle(-25, 0, -250, 400).setStrokeStyle(2, 0xffffff).setFillStyle(0x333333);
+    const statsBg = this.scene.add
+      .rectangle(-25, 0, -250, 400)
+      .setStrokeStyle(2, 0xffffff)
+      .setFillStyle(0x333333);
     const container = this.scene.add.container(x, y, [statsBg]);
-    container.setInteractive(new Phaser.Geom.Rectangle(-175, -290, 200, 400), Phaser.Geom.Rectangle.Contains);
+    container.setInteractive(
+      new Phaser.Geom.Rectangle(-175, -290, 200, 400),
+      Phaser.Geom.Rectangle.Contains
+    );
 
-    const healthText = this.scene.add.text(-30, -150, `Vida: ${health}`, this.textStyle).setOrigin(0.5).setFontSize(36);
-    const damageText = this.scene.add.text(-30, -75, `Dano: ${damage}`, this.textStyle).setOrigin(0.5).setFontSize(36);
+    const healthText = this.scene.add
+      .text(-30, -150, `Vida: ${health}`, this.textStyle)
+      .setOrigin(0.5)
+      .setFontSize(36);
+    const damageText = this.scene.add
+      .text(-30, -75, `Dano: ${damage}`, this.textStyle)
+      .setOrigin(0.5)
+      .setFontSize(36);
     const moveSpeedText = this.scene.add
       .text(-30, 0, `Velocidade: ${moveSpeed}`, this.textStyle)
       .setOrigin(0.5)
       .setFontSize(28);
 
-    const luckyText = this.scene.add.text(-30, 75, `Sorte: ${lucky}`, this.textStyle).setOrigin(0.5).setFontSize(36);
+    const luckyText = this.scene.add
+      .text(-30, 75, `Sorte: ${lucky}`, this.textStyle)
+      .setOrigin(0.5)
+      .setFontSize(36);
 
     container.add([healthText, damageText, moveSpeedText, luckyText]);
   }
