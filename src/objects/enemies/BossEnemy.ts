8d577a9574cb2ff2592ad5c55d27d9181d6a01a2
rmvllo <romulodtsn@gmail.com>
2025-05-09 16:53:03 -0300

    feat(bossenemy): bossEnemy now shoots a projectile(tree)

--- src/objects/enemies/BossEnemy.ts
+++ src/objects/enemies/BossEnemy.ts
@@ -4,7 +4,7 @@ import { Player } from "../player/playerObject";
 
 export class BossEnemy extends Phaser.Physics.Arcade.Sprite {
   #player: Player;
-  #lastStableDirection: string = 'down'; // Guarda a última direção estável para evitar flickering
+  public bulletGroup: Phaser.Physics.Arcade.Group;
 
   constructor(scene: Phaser.Scene, x: number, y: number, player: Player) {
     super(scene, x, y, 'boss');
@@ -13,25 +13,40 @@ export class BossEnemy extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    this.setScale(4);
+    this.setScale(6);
     this.setSize(48, 48);
     this.setDepth(20);
     this.setCollideWorldBounds(true);
+    this.bulletGroup = this.scene.physics.add.group();
+    this.setupContinuousShooting(scene);
+  
 
     const bossId = `boss_${Date.now()}_${Math.random().toString(16).slice(2)}`;
     const bossHealth = new HealthComponent(currentEnemyStats.BossEnemy.Health, currentEnemyStats.BossEnemy.Health, bossId);
     this.setData('healthComponent', bossHealth);
 
   }
-
-  update(time: number, delta: number) {
-    if (!this.active) return;
   
-    this.scene.physics.moveToObject(this, this.#player, currentEnemyStats.BossEnemy.Speed);
-    // this.play('boss_left',true)
-    // this.#playDirectionalAnim();
+  private setupContinuousShooting(scene: Phaser.Scene) {
+    scene.time.addEvent({
+      delay: currentEnemyStats.BossEnemy.FireRate,
+      loop:true,
+      callback: () => {
+        if (this.active && this instanceof Phaser.Physics.Arcade.Sprite) {
+          const bullet = this.scene.physics.add.sprite(this.x, this.y, 'bossBullet');
+          bullet.setOffset(0.5,0.5)
+          this.bulletGroup.add(bullet);
+          scene.physics.moveToObject(bullet, this.#player, currentEnemyStats.BossEnemy.BulletSpeed);
+        }
+      }
+    })
   }
-  
+
+public updateEnemyBossMovement(scene:Phaser.Scene) {
+  this.scene.physics.moveToObject(this, this.#player, currentEnemyStats.BossEnemy.Speed);
+}
+
+
   public takeDamage(amount: number): void {
     if (!this.active) return;
 
@@ -60,32 +75,4 @@ export class BossEnemy extends Phaser.Physics.Arcade.Sprite {
     this.destroy();
   }
 
-  #playDirectionalAnim() {
-    const dx = this.#player.x - this.x;
-    const dy = this.#player.y - this.y;
-  
-    let direction = this.#lastStableDirection;
-  
-    if (Math.abs(dx) > Math.abs(dy)) {
-      direction = dx > 0 ? 'right' : 'left';
-    } else {
-      direction = dy > 0 ? 'down' : 'up';
-    }
-  
-    const animKey = `boss_${direction}`;
-    const currentAnim = this.anims.currentAnim?.key;
-  
-    if (currentAnim !== animKey && this.scene.anims.exists(animKey)) {
-      console.log(`Tocando animação: ${animKey}`);
-      this.stop(); // Parar a animação anterior
-      this.play(animKey, true); // Reproduzir a nova animação
-      this.#lastStableDirection = direction;
-    } else if (currentAnim === animKey) {
-      console.log(`Já tocando a animação: ${currentAnim}`);
-    }
-  }
-  
-  
-  
-  
 }
